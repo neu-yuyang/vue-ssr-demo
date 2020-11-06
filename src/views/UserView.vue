3aae0e4de100dcba6d9589da4f0fb0e207d9528f
uzumaki naruto <neu_yuyang@163.com>
2020-11-06 19:04:16 +0800

    add vue-ssr-demo

---
+++ src/views/UserView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="user-view">
+    about
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'user-view',
+
+  computed: {
+    user () {
+      // return this.$store.state.users[this.$route.params.id]
+      return false
+    }
+  },
+
+  asyncData ({ store, route: { params: { id }}}) {
+    return store.dispatch('FETCH_USER', { id })
+  },
+
+  title () {
+    return this.user
+      ? this.user.id
+      : 'User not found'
+  }
+}
+</script>
+
+<style lang="stylus">
+.user-view
+  background-color #fff
+  box-sizing border-box
+  padding 2em 3em
+  h1
+    margin 0
+    font-size 1.5em
+  .meta
+    list-style-type none
+    padding 0
+  .label
+    display inline-block
+    min-width 4em
+  .about
+    margin 1em 0
+  .links a
+    text-decoration underline
+</style>
